89a2bc7c4fc71fdaaf538373532775bac975094b
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-05-26 16:44:07 -0300

    terminando p1

--- personagem1.php
+++ personagem1.php
@@ -14,4 +14,19 @@ require_once __DIR__ . "/lang/" . $_SESSION['lang'] . ".php";
     <link rel="stylesheet" href="assets/css/dark-mode.css" id="dark-mode-css" disabled>
     <link rel="stylesheet" href="assets/css/daltonismo-prot.css" id="prot-css" disabled>
     <link rel="stylesheet" href="assets/css/daltonismo-deut.css" id="deut-css" disabled>
- 
+    <link rel="stylesheet" href="assets/css/daltonismo-trit.css" id="trit-css" disabled>
+</head>
+<body>
+    <div class="personagem-container">
+        <div class="imagem-personagem" style="background-color: #ffffff;">
+            <img src="assets/img/personagem1.png" alt="My Melody" style="width: 100%; height: auto;">
+        </div>
+        <div class="descricao-personagem" style="background-color: #f9cce3; color: #333; padding: 2rem;">
+            <h1>My Melody</h1>
+            <p><?php echo $lang['my_melody_description'] ?? 'Uma coelhinha doce e gentil que adora fazer doces e passar o tempo com seus amigos.'; ?></p>
+            <a href="index.php">&larr; <?php echo $lang['back']; ?></a>
+        </div>
+    </div>
+    <script src="assets/js/accessibility.js"></script>
+</body>
+</html>
