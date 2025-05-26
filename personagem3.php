c2c5112232963e499b237366963ffd930699d2eb
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-05-26 16:46:05 -0300

    p3 crianod

---
+++ personagem3.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if (!isset($_SESSION['lang'])) {
+    $_SESSION['lang'] = 'pt';
+}
+require_once __DIR__ . "/lang/" . $_SESSION['lang'] . ".php";
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $_SESSION['lang']; ?>">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Cinnamoroll</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/dark-mode.css" id="dark-mode-css" disabled />
+    <link rel="stylesheet" href="assets/css/daltonismo-prot.css" id="prot-css" disabled />
+    <link rel="stylesheet" href="assets/css/daltonismo-deut.css" id="deut-css" disabled />
