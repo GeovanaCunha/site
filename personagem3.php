6c7a41977b3cdf706fece4b0e55a7b4d2d469977
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-05-26 16:46:16 -0300

    p3 terminando

--- personagem3.php
+++ personagem3.php
@@ -15,3 +15,19 @@ require_once __DIR__ . "/lang/" . $_SESSION['lang'] . ".php";
     <link rel="stylesheet" href="assets/css/dark-mode.css" id="dark-mode-css" disabled />
     <link rel="stylesheet" href="assets/css/daltonismo-prot.css" id="prot-css" disabled />
     <link rel="stylesheet" href="assets/css/daltonismo-deut.css" id="deut-css" disabled />
+    <link rel="stylesheet" href="assets/css/daltonismo-trit.css" id="trit-css" disabled />
+</head>
+<body>
+    <div class="personagem-container">
+        <div class="imagem-personagem" style="background-color: #ffffff;">
+            <img src="assets/img/personagem3.png" alt="Cinnamoroll" style="width:100%; height:auto;" />
+        </div>
+        <div class="descricao-personagem" style="background-color: #a7d8f0; color: #333; padding: 2rem;">
+            <h1>Cinnamoroll</h1>
+            <p><?php echo $lang['cinnamoroll_description'] ?? 'Um cachorrinho fofo e leal, tímido mas muito amigável.'; ?></p>
+            <a href="index.php">&larr; <?php echo $lang['back']; ?></a>
+        </div>
+    </div>
+    <script src="assets/js/accessibility.js"></script>
+</body>
+</html>
